f0622363d9f29a50ee17e1c5f89c35c0e129bcba
zakwanzulkifli0770-ship-it <zakwanzulkifli0770@gmail.com>
2026-09-04 20:11:40 +0800

    Add Vercel Analytics to layout

--- next.js
+++ next.js
@@ -1,1 +1,12 @@
+import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+
+export default function Layout({ children }) {
+	return (
+		<>
+			{children}
+			<Analytics />
+			<SpeedInsights />
+		</>
+	)
+}
